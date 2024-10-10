4496e0f2a72944abcaba5876eb875753b830a7fb
Executioner <conscious00000@gmail.com>
2024-10-10 16:50:45 +0800

    feat: Expand Carnant's abilities and add mutant form
    This commit enhances the Carnant character with new abilities and a mutant form:
    
    - Added a
     mutant form for Carnant, triggered upon death in human form.
    - Expanded Carnant's skillset with 4 new abilities: Tentacle Pierce, Venom Puncture, Poison Regen, and Dissolve Armor.
    - Implemented logic for these new abilities, including effects like piercing defense, applying venom damage over time,
     healing, and reducing target's defense.
    - Updated Carnant's attack logic to utilize different skills based on current form.
    - Adjusted Carnant's stats and health regeneration in mutant form.
    - Added venom and dissolve armor status effects.

--- app/src/main/java/com/example/dreadful/characters/Carnant.java
+++ app/src/main/java/com/example/dreadful/characters/Carnant.java
@@ -23,14 +23,21 @@ public class Carnant extends Player {
     private TextView yourName;
     private int form = 0;
     private int increaseHeal = 4, maxIncreaseHeal = 4;
-    private int heal = 450;
+    private int increaseVenom = 4, maxIncreaseVenom = 4;
+    private int heal = 450, venom = 100;
+    private int dissolve = 0;
 
     public Carnant(Context context, ImageView yourImage, ProgressBar yourHealthBar, TextView yourName) {
         super(context, yourImage, "This Psycho", R.drawable.character_psychopath, "left", 140,
                 new int[]{R.drawable.character_carnant}, null,
                 2100, 180, 10, 40,
-                new String[]{"Bat Slam", "Hard Swing", "Left Kick"},
-                new int[]{0, 4, 2}, new int[]{0, 0, 0});
+                new String[]{"Bat Slam", "Hard Swing", "Left Kick",
+                        "Tentacle Pierce", "Venom Puncture", "Poison Regen", "Dissolve Armor"},
+                new int[]{0, 4, 2,
+                        0, 1, 5, 4},
+
+                new int[]{0, 0, 0,
+                        0, 0, 0, 0});
 
         this.yourImage = yourImage;
         this.yourHealthBar = yourHealthBar;
@@ -53,8 +60,7 @@ public class Carnant extends Player {
         hitter.setAttack(hitter.getMaxAttack());
         yourImage.startAnimation(shakeAnimation);
 
-        if(form == 0)
-        {
+        if (form == 0) {
             if (getHealth() <= 0) {
                 int toTransform = random.nextInt(2);
 
@@ -127,8 +133,7 @@ public class Carnant extends Player {
         setDamageOverTime(tempDot);
         setDamageOverTimeValue(tempDotValue);
 
-        if(form == 0)
-        {
+        if (form == 0) {
             if (getHealth() <= 0) {
                 int toTransform = random.nextInt(2);
 
@@ -145,11 +150,9 @@ public class Carnant extends Player {
     }
 
     public void receiveTimeEffect(Player hitter, Player target) {
-        if(form == 1)
-        {
+        if (form == 1) {
             bypassSetHealth(getHealth() + heal);
-            if(getHealth() > getMaxHealth())
-            {
+            if (getHealth() > getMaxHealth()) {
                 bypassSetMaxHealth(getHealth());
                 yourHealthBar.setMax(getMaxHealth());
             }
@@ -161,19 +164,48 @@ public class Carnant extends Player {
                 increaseHeal = maxIncreaseHeal;
                 heal += 80;
             }
+
+            increaseVenom--;
+            if (increaseVenom <= 0) {
+                increaseVenom = maxIncreaseVenom;
+                venom += 80;
+            }
+
+            dissolve--;
+            if (dissolve <= 0)
+            {
+                if(!hasStatus(target, "Dissolve Armor", 100).isEmpty())
+                {
+                    setDefense(getMaxDefense());
+
+                    int index = Integer.parseInt(hasStatus(target, "Dissolve Armor", 100));
+                    getStatusValue().remove(index);
+                    getStatus().remove(index);
+                }
+            }
         }
     }
 
     public String useRandomAttack(Player hitter, Player target) {
         String skillName;
-        int skillIndex = random.nextInt(getSkillNames().length);
+        int skillIndex;
 
-        while (getSkillCooldowns()[skillIndex] > 0) {
-            skillIndex = random.nextInt(getSkillNames().length);
+        if (getSkillCooldowns()[4] > 0) {
+            getSkillCooldowns()[4] = 0;
         }
+
+        do {
+            if (form == 0) {
+                skillIndex = random.nextInt(3);
+            } else {
+                skillIndex = random.nextInt(3) + 3;
+            }
+            //skillIndex = random.nextInt(getSkillNames().length);
+        } while (getSkillCooldowns()[skillIndex] > 0);
 
         skillName = getSkillNames()[skillIndex];
         switch (skillIndex) {
+            //human form
             case 0:
                 basicAttack(hitter, target);
                 break;
@@ -182,6 +214,20 @@ public class Carnant extends Player {
                 break;
             case 2:
                 skill2(hitter, target);
+                break;
+
+            //mutant form
+            case 3:
+                skill3(hitter, target);
+                break;
+            case 4:
+                skill4(hitter, target);
+                break;
+            case 5:
+                skill5(hitter, target);
+                break;
+            case 6:
+                skill6(hitter, target);
                 break;
         }
 
@@ -212,5 +258,48 @@ public class Carnant extends Player {
     //simple attack
     private void skill2(Player hitter, Player target) {
         target.receiveHit(hitter, target);
+    }
+
+    //attack that will pierce through defense
+    private void skill3(Player hitter, Player target) {
+        target.setDefense(0);
+        setAttack(getAttack() * 2);
+
+        target.receiveHit(hitter, target);
+
+        setAttack(getMaxAttack());
+        target.setDefense(target.getMaxDefense());
+    }
+
+    //same effect with basic attack but with enhanced damage and follow up damage over time that last for 4 turns
+    private void skill4(Player hitter, Player target) {
+        target.setDefense(0);
+        setAttack(getAttack() * 4);
+
+        target.receiveHit(hitter, target);
+
+        setAttack(getMaxAttack());
+        target.setDefense(target.getMaxDefense());
+
+        target.getDamageOverTime().add(venom);
+        target.getDamageOverTimeValue().add(12);
+    }
+
+    //simple heal over time that last for 4 turns while also applying venom that last for 4 turns
+    private void skill5(Player hitter, Player target) {
+        getHealOverTime().add(heal * 4);
+        getHealOverTimeValue().add(12);
+
+        target.getDamageOverTime().add(venom);
+        target.getDamageOverTimeValue().add(12);
+    }
+
+    //increase dodge while applying dissolve armor status and venom that last for 4 turns
+    private void skill6(Player hitter, Player target) {
+        setDodge(getDodge() + 80);
+        receiveStatus(hitter, "Dissolve Armor", 100);
+
+        target.getDamageOverTime().add(venom);
+        target.getDamageOverTimeValue().add(12);
     }
 }
