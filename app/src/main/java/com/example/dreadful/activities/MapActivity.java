93d90834272b45fdc89582ff1d173d592f9f21be
Executioner <conscious00000@gmail.com>
2024-11-07 14:44:37 +0800

    feat: Add new map "Stature" and improve map list UI
    This commit introduces a new map called "Stature" and enhances the map list UI
    :
    
    - Added "Stature" to the map list with its corresponding background image.
    - Adjusted the map list layout to wrap its content instead of having fixed dimensions.
    - Decreased the map name text size in the map list view for better readability.

--- app/src/main/java/com/example/dreadful/activities/MapActivity.java
+++ app/src/main/java/com/example/dreadful/activities/MapActivity.java
@@ -43,6 +43,7 @@ public class MapActivity extends AppCompatActivity {
         huntButton = findViewById(R.id.huntButton);
 
         mapListArray.add(new Map("Shadowgrove", 1, R.drawable.background_dark_forest, 0));
+        mapListArray.add(new Map("Stature", 0, R.drawable.background_statue, 0));
         mapListArray.add(new Map("Hell", 0, R.drawable.background_hell, 0));
 
         LinearLayoutManager statusLayoutManager = new LinearLayoutManager(this, LinearLayoutManager.VERTICAL, false);
